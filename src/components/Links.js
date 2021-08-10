9141ed4c6e9b74744165711582953e3d0d16288c
jakob <jakob.germano@gmail.com>
2021-08-09 18:39:49 -0600

    Completed assignment

---
+++ src/components/Links.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Links(props) {
+
+  return (
+    <div id="links">
+      <h2>Links</h2>
+      <a href={props.github}>{props.github}</a>
+      <a href={props.linkedin}> {props.linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
